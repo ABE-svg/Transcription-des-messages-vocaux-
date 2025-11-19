de3f18489664a5139890981b592cbc9d7961590e
Yasmine <Yasminealuch44@gmail.com>
2025-11-19 13:43:38 +0100

    Ajout du fichier requirements et du script de mise en place de l'environnement virtuel

---
+++ setup_env.sh
@@ -0,0 +1,24 @@
+set -e
+
+# Détection de python en fonction de l'OS
+if command -v python3 &>/dev/null; then
+    PYTHON=python3
+elif command -v python &>/dev/null; then
+    PYTHON=python
+elif command -v py &>/dev/null; then
+    PYTHON="py"
+else
+    echo "Aucun interpréteur Python trouvé."
+    exit 1
+fi
+
+$PYTHON -m venv .venv
+
+source .venv/Scripts/activate 2>/dev/null || source .venv/bin/activate
+
+$PYTHON -m pip install --upgrade pip
+$PYTHON -m pip install -r requirements.txt
+
+
+echo "Environnement prêt. Pour l'activer exécute : source .venv/bin/activate"
+echo "source .venv/bin/activate"
